2b9bba4ef1511cb808b72b3aa3b1a8e0e07d7da1
Ricardo Cósero Basso <94764850+GoldenerReiter@users.noreply.github.com>
2022-11-03 03:06:43 -0300

    booleans

--- week1/2_dataTypes.js
+++ week1/2_dataTypes.js
@@ -1,8 +1,11 @@
 var name = "John"; // this is a string datatype. a string is text, you can save to it even numbers, but JS will se text
 var age = 47; // this is an int datatype. int are numbers.
 var salute = "Hello, "; // this is also a string datatype
+var boolean = true; // a boolean datatype is the one that can be only true or false.
+var boolean1 = false;
 
-console.log(salute, name, "Your age is: ", age) // console is a javascript class. log is a method of the console class
+console.log(salute, name, "Your age is: ", age, "and life is", boolean, "and not", boolean1)
+                                                // console is a javascript class. log is a method of the console class
                                                 // console.log means that you want to represent some info of that var.
                                                 // methods and functions use () for initializing and adding to it some
                                                 // parameters. like when you say "console.log(name);" you're giving
---
+++ week1/5_strings.js
@@ -0,0 +1,8 @@
+// strings are a collection of characters.
+// strings are assigned by two types of characters. "" and ''.
+var singleQuote = 'Hello';
+var doubleQuote = "Mate";
+// always remember that a string is always wrapped by two quotation marks.
+
+console.log(singleQuote, doubleQuote);
+
