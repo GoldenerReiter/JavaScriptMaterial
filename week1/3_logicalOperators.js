814709d00ee1c7ad619773a46929acbcb9c481f7
Ricardo Cósero Basso <94764850+GoldenerReiter@users.noreply.github.com>
2022-11-03 02:48:12 -0300

    logicalOperators2

--- week1/3_logicalOperators.js
+++ week1/3_logicalOperators.js
@@ -6,6 +6,9 @@ var judgement1 = "a priori";
 var kant = judgement && judgement1; // there's && for and, || for or, ! for not.
 var compare = 5 > 1;                // you can compare two statements too
 
+var number1 = 5;
+var number2 = "5";
+
 if (kant) {  // if kant is true, then...
     console.log("You judgement is", judgement, judgement1); // the console will say this.
 }
@@ -13,6 +16,15 @@ if (kant) {  // if kant is true, then...
 if (compare) {
     console.log("5 is greater than 1");
 }
+
+if (number1 == number2) { // type coercion comparison
+    console.log("This text will log");  // this logs because == means for the browser that you want to compare 5 and 5
+                                        // ignoring the datatype of the two values.
+}
+if (number1 === number2) { // strict equality comparison
+    console.log("This will NOT log!");
+}
+
 
 
 let magic = document.querySelector("h1"); // please ignore the magic for now!
--- week1/4_numbers.js
+++ week1/4_numbers.js
@@ -1,4 +1,13 @@
+var addition = 1 + 1;
+var substraction = 1 - 1;
+var multiplication = 2 * 3;
+var division = 10 / 2;
 
+
+
+
+console.log();
+console.log();
 
 
 let magic = document.querySelector("h1"); // please ignore the magic for now!
