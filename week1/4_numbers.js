57fa6c68e0dac9d6e083a7c4fcba6fd65295dd24
Ricardo Cósero Basso <94764850+GoldenerReiter@users.noreply.github.com>
2022-11-03 02:57:59 -0300

    pemdas

--- week1/4_numbers.js
+++ week1/4_numbers.js
@@ -2,11 +2,20 @@ var addition = 1 + 1;
 var substraction = 1 - 1;
 var multiplication = 2 * 3;
 var division = 10 / 2;
-                            // if you know mathematics, this will be self-evident
+var powerOf = 10**2;
+var moduleOf = 10 % 5; // this represents the remainder of a division!
+var moduleOf1 = 10 % 3;
+var pemdas = (5*2/2+1)*2-1; // can you guess the result of this operation?
+// if you know mathematics, this will be self-evident
+
 console.log(addition);
 console.log(substraction);
 console.log(multiplication);
 console.log(division);
+console.log(powerOf);
+console.log(moduleOf);
+console.log(moduleOf1);
+console.log(pemdas);
 
 // also you have number types.
 
